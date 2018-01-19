96a4fe170e748df4b00b1570ec02437b3498d5e7
Xapier <lancecrisang14@gmail.com>
2018-01-19 18:18:56 +0800

    Edited version number

--- Skript-Interpreter/Properties/AssemblyInfo.cs
+++ Skript-Interpreter/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("sk_interp")]
+[assembly: AssemblyTitle("Skript Interpreter")]
 [assembly: AssemblyDescription("sKript Interpreter & Compiler")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Lance Crisang")]
@@ -33,7 +33,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyVersion("0.3.0.0")]
 [assembly: AssemblyFileVersion("0.2.0.0")]
 [assembly: NeutralResourcesLanguage("en")]
 
